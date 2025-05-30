5c65a940e90b0dae01f26856680e928ba390f786
Cesar Meche <cesar.lizarazo02@uptc.edu.co>
2025-05-30 10:55:12 -0500

    main

---
+++ src/main/java/co/edu/uptc/server/Main.java
@@ -0,0 +1,13 @@
+package co.edu.uptc.server;
+
+import co.edu.uptc.server.controller.Controller;
+
+
+public class Main {
+    public static void main(String[] args) {
+        Controller c= new Controller();
+        c.initCinemaSystem();
+        
+
+    }
+}
